681b2966331b0e24f70a232803cbebd441562c53
anshawuqing <1748896856@qq.com>
2018-04-12 08:29:14 +0800

    model

---
+++ Assets/DesignPattern/Proxy/Prototype.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+
+
+/// <summary>
+///定义原型模式的抽象类
+/// </summary>
+ abstract    class Prototype
+    {
+
+    private string id;
+    
+
+    /// <summary>
+    /// 构造函数
+    /// </summary>
+    /// <param name="id"></param>
+    public Prototype (string id)
+    {
+        this.id = id;
+    }
+
+   public string Id
+    {
+          get { return id; }
+    }
+
+    /// <summary>
+    ///定义  克隆方法  在 具体原型类中进行克隆
+    /// </summary>
+    /// <returns></returns>
+    public abstract Prototype Clone();
+    }
+
+
+
---
+++ Assets/DesignPattern/SimpleFactery/Operation.cs
@@ -0,0 +1,141 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+///   简单  工厂模式
+/// </summary>
+public class Operation  {
+
+
+    /// <summary>
+    ///
+    /// </summary>
+    /// <param name="numberA"></param>
+    /// <param name="numberB"></param>
+    /// <param name="operate"></param>
+ //   /// <returns></returns>
+	//public  static  double GetResult(double  numberA, double  numberB, string operate)
+ //   {
+ //       double result = 0d;
+
+ //       switch ( operate)
+ //       {
+
+ //           case "+":
+ //               {
+ //                   result = numberA + numberB;
+ //               }break;
+ //           case "-":
+ //               {
+ //                   result = numberA - numberB;
+ //               }break;
+ //            case "*":
+ //               {
+ //                   result = numberA * numberB;
+ //               }break;
+ //             case "/":
+ //               {
+ //                   result = numberA / numberB;
+ //               }break;
+                          
+ //           default:
+ //               break;
+ //       }
+
+ //       return result;
+
+ //   }
+
+
+    private double _numberA = 0;
+
+    private double _numberB = 0;
+
+    /// <summary>
+    /// 设置属性A 
+    /// </summary>
+    public double NumberA
+    {
+          get { return _numberA; }
+           set { _numberB = value; }
+     }
+
+
+
+    /// <summary>
+    /// 设置属性B
+    /// </summary>
+    public  double NumberB
+    {
+        get { return _numberB; }
+        set { _numberB = value; }
+       
+    }
+  
+
+
+  /// <summary>
+  ///虚方法  在 子类中进行重写
+  /// </summary>
+  public virtual   double  GetResult()
+    {
+        double result = 0;
+        return result;
+    }
+}
+
+/// <summary>
+/// 加法雷
+/// </summary>
+class OperationAdd:Operation
+{
+    public override double GetResult()
+    {
+        double result = 0;
+        result = NumberA + NumberB;
+        return result;
+    }
+
+}
+
+
+/// <summary>
+/// 减法类
+/// </summary>
+class OperationSub:Operation
+{
+    public override double GetResult()
+    {
+        double result = 0;
+        result = NumberA - NumberB;
+        return result;
+    }
+}
+
+
+/// <summary>
+///乘法类
+/// </summary>
+class OperationMul:Operation
+{
+    public override double GetResult()
+    {
+        double result = 0;
+        result = NumberA * NumberB;
+        return result;
+    }
+}
+
+class OperationDiv:Operation
+{
+    public override double GetResult()
+    {
+        double result = 0;
+        if (NumberB == 0)
+            Debug.Log("除数不能为零");
+        result = NumberA / NumberB;
+        return result;
+    }
+}
+
