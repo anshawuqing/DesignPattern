26e906ab56be8911bb0d368c7bd51226b4530a3d
杨万盛 <1748896856@qq.com>
2018-05-09 18:39:01 +0800

    桥接模式

---
+++ Assets/DesignPattern/Iterator/IteratorMain.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// 
+/// </summary>
+public class IteratorMain
+{
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/DesignPattern/Singleton/Singleton.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// 单利模式
+/// </summary>
+ class Singleton
+{
+    /// <summary>
+    ///单利模式
+    /// </summary>
+    private static Singleton instance;
+
+      /// <summary>
+   ///私有化 构造函数 防止外界进行NEW    的操作 /// </summary>
+   private     Singleton ()
+        {
+
+        }
+ 
+    /// <summary>
+    ///  
+    /// </summary>
+    /// <returns></returns>
+    public  static Singleton  GetInstance()
+    {
+        if (instance == null )
+        {
+            instance = new Singleton();
+        }
+        
+        return instance;
+    }
+    
+} 
